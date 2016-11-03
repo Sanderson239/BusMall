e34d60bd4351e061afa14349e2c9fbc9d0d283b4
Stephen Anderson <sa3196a@student.american.edu>
2016-11-03 14:12:05 -0700

    added chart.js. put in a placeholder chart to understand how those things work

--- app.js
+++ app.js
@@ -98,6 +98,59 @@ function clickImage(event) {
     }
   }
 }
+var buyerData = {
+  labels : ['January','February','March','April','May','June'],
+  datasets : [
+    {
+      fillColor : 'rgba(172,194,132,0.4)',
+      strokeColor : '#ACC26D',
+      pointColor : '#fff',
+      pointStrokeColor : '#9DB86D',
+      data : [203,156,99,251,305,247]
+    }
+  ]
+};
+
+// var buyers = document.getElementById('buyers').getContext('2d');
+// new Chart(buyers).Line(buyerData);
+// var myChart = new Chart(buyers, { data: buyerData });
+var ctx = document.getElementById("buyers").getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+      }]
+  },
+  options: {
+      scales: {
+          yAxes: [{
+              ticks: {
+                  beginAtZero:true
+                }
+            }]
+        }
+    }
+});
 
 // function setImageAttributes() {
 //   for (var i = 0; i < document.getElementsByClassName('products').length; i++) {
